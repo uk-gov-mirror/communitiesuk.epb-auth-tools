cc51ebc35b34a0867d4d6ab9c8b9d7d1d86c5cdf
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2019-12-09 16:54:00 +0000

    Refactor: move testing tokens to spec_helper
    Co-authored-by: Yusuf <yusuf@madetech.com>
    Co-authored-by: Jaseera <jaseera@madetech.com>

--- spec/acceptance/validate_jwt_token_spec.rb
+++ spec/acceptance/validate_jwt_token_spec.rb
@@ -12,63 +12,24 @@ describe Auth::TokenProcessor do
     @token_processor = Auth::TokenProcessor.new @jwt_secret, @jwt_issuer
 
     @malformed_token = Base64.encode64 'I am all wrong'
-
-    @incorrect_issuer_token =
-      token_generate @jwt_secret,
-                     exp: Time.now.to_i + 60 * 60,
-                     iat: Time.now.to_i,
-                     iss: 'incorrect.issuer',
-                     sub: uuid_generate,
-                     scopes: %w[scope:1 scope:2 scope:3]
-
-    @expired_token =
-      token_generate @jwt_secret,
-                     exp: Time.now.to_i - 60 * 60,
-                     iat: Time.now.to_i,
-                     iss: @jwt_issuer,
-                     sub: uuid_generate,
-                     scopes: %w[scope:1 scope:2 scope:3]
-
-    @premature_token =
-      token_generate @jwt_secret,
-                     exp: Time.now.to_i + 60 * 60,
-                     iat: Time.now.to_i + 30 * 60,
-                     iss: @jwt_issuer,
-                     sub: uuid_generate,
-                     scopes: %w[scope:1 scope:2 scope:3]
-
-    @no_sub_token =
-      token_generate @jwt_secret,
-                     exp: Time.now.to_i + 60 * 60,
-                     iat: Time.now.to_i,
-                     iss: @jwt_issuer,
-                     scopes: %w[scope:1 scope:2 scope:3]
-
-    @valid_token =
-      token_generate @jwt_secret,
-                     exp: Time.now.to_i + 60 * 60,
-                     iat: Time.now.to_i,
-                     iss: @jwt_issuer,
-                     sub: uuid_generate,
-                     scopes: %w[scope:1 scope:2 scope:3]
   end
 
   context 'when a token is valid' do
     it 'does return an instance of Auth::Token' do
-      expect(@token_processor.process(@valid_token)).to be_an_instance_of(
-        Auth::Token
-      )
+      expect(
+        @token_processor.process(token_generate(@jwt_secret, :valid_token))
+      ).to be_an_instance_of(Auth::Token)
     end
 
     it 'does allow checking a scope' do
-      token = @token_processor.process @valid_token
+      token = @token_processor.process token_generate(@jwt_secret, :valid_token)
 
       expect(token.scope?('scope:1')).to be true
       expect(token.scope?('does-not-exist')).to be false
     end
 
     it 'does allow checking a number of scopes' do
-      token = @token_processor.process @valid_token
+      token = @token_processor.process token_generate(@jwt_secret, :valid_token)
 
       expect(token.scopes?(%w[scope:1 scope:2])).to be true
       expect(token.scope?(%w[scope:1 does-not-exist])).to be false
@@ -86,32 +47,35 @@ describe Auth::TokenProcessor do
   context 'when a token has a different issuer' do
     it 'does throw an Auth::TokenHasWrongIssuer Error' do
       expect {
-        @token_processor.process @incorrect_issuer_token
+        @token_processor.process token_generate(
+                                   @jwt_secret,
+                                   :incorrect_issuer_token
+                                 )
       }.to raise_error(instance_of(Auth::TokenHasWrongIssuer))
     end
   end
 
   context 'when a token has expired' do
     it 'does throw an Auth::TokenExpired Error' do
-      expect { @token_processor.process @expired_token }.to raise_error(
-        instance_of(Auth::TokenExpired)
-      )
+      expect {
+        @token_processor.process token_generate(@jwt_secret, :expired_token)
+      }.to raise_error(instance_of(Auth::TokenExpired))
     end
   end
 
   context 'when a token was issued in the future' do
     it 'does throw an Auth::TokenNotYetValid Error' do
-      expect { @token_processor.process @premature_token }.to raise_error(
-        instance_of(Auth::TokenNotYetValid)
-      )
+      expect {
+        @token_processor.process token_generate(@jwt_secret, :premature_token)
+      }.to raise_error(instance_of(Auth::TokenNotYetValid))
     end
   end
 
   context 'when a token does not have a subject' do
     it 'does throw an Auth::TokenHasNoSubject Error' do
-      expect { @token_processor.process @no_sub_token }.to raise_error(
-        instance_of(Auth::TokenHasNoSubject)
-      )
+      expect {
+        @token_processor.process token_generate(@jwt_secret, :no_sub_token)
+      }.to raise_error(instance_of(Auth::TokenHasNoSubject))
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,5 +7,42 @@ def uuid_generate
 end
 
 def token_generate(secret, payload)
-  JWT.encode payload, secret, 'HS256'
+  payloads = {
+    incorrect_issuer_token: {
+      exp: Time.now.to_i + 60 * 60,
+      iat: Time.now.to_i,
+      iss: 'incorrect.issuer',
+      sub: uuid_generate,
+      scopes: %w[scope:1 scope:2 scope:3]
+    },
+    expired_token: {
+      exp: Time.now.to_i - 60 * 60,
+      iat: Time.now.to_i,
+      iss: @jwt_issuer,
+      sub: uuid_generate,
+      scopes: %w[scope:1 scope:2 scope:3]
+    },
+    premature_token: {
+      exp: Time.now.to_i + 60 * 60,
+      iat: Time.now.to_i + 30 * 60,
+      iss: @jwt_issuer,
+      sub: uuid_generate,
+      scopes: %w[scope:1 scope:2 scope:3]
+    },
+    no_sub_token: {
+      exp: Time.now.to_i + 60 * 60,
+      iat: Time.now.to_i,
+      iss: @jwt_issuer,
+      scopes: %w[scope:1 scope:2 scope:3]
+    },
+    valid_token: {
+      exp: Time.now.to_i + 60 * 60,
+      iat: Time.now.to_i,
+      iss: @jwt_issuer,
+      sub: uuid_generate,
+      scopes: %w[scope:1 scope:2 scope:3]
+    }
+  }
+
+  JWT.encode payloads[payload], secret, 'HS256'
 end
