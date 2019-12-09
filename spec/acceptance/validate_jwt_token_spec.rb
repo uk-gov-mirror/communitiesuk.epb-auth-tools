dc8aa584b2011c02384ca5104fe30b5eae4293b8
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2019-12-09 11:09:51 +0000

    Refactor: Use instance variable instead of local variable
    Co-authored-by: Yusuf <yusuf@madetech.com>
    Co-authored-by: Jaseera <jaseera@madetech.com>

--- spec/acceptance/validate_jwt_token_spec.rb
+++ spec/acceptance/validate_jwt_token_spec.rb
@@ -12,34 +12,30 @@ describe Auth::Token do
     @malformed_token = Base64.encode64('I am all wrong')
     @incorrect_issuer_token = token_generate @jwt_secret, 'not.an.issuer'
     @expired_token = token_generate @jwt_secret, @jwt_issuer, Time.now.to_i - 60 * 60
+
+    @token = Auth::Token.new @jwt_secret, @jwt_issuer
   end
 
   context 'when a token is malformed' do
     it 'does throw an Auth::TokenMalformed Error' do
-      token = Auth::Token.new @jwt_secret, @jwt_issuer
-
       expect do
-        token.process @malformed_token
+        @token.process @malformed_token
       end.to raise_error(instance_of(Auth::TokenMalformed))
     end
   end
 
   context 'when a token has a different issuer' do
     it 'does throw an Auth::WrongIssuer Error' do
-      token = Auth::Token.new @jwt_secret, @jwt_issuer
-
       expect do
-        token.process @incorrect_issuer_token
+        @token.process @incorrect_issuer_token
       end.to raise_error(instance_of(Auth::WrongIssuer))
     end
   end
 
   context 'when a token has expired' do
     it 'does throw an Auth::TokenExpired Error' do
-      token = Auth::Token.new @jwt_secret, @jwt_issuer
-      puts @expired_token
       expect do
-        token.process @expired_token
+        @token.process @expired_token
       end.to raise_error(instance_of(Auth::TokenExpired))
     end
   end
